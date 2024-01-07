cf52d2cc96b424a9a3b329d6970c8cb029f3c2fc
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-01-07 22:47:02 +0530

    direct case page working correctly

--- src/Components/DirectCases/directcases.jsx
+++ src/Components/DirectCases/directcases.jsx
@@ -1,21 +1,25 @@
-import React, { useState, useEffect, useParams } from "react";
+import React, { useState, useEffect } from "react";
 import "./directcases.css";
 import axios from "axios";
 import { FiDownload } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
 function DirectCases() {
-  // const {disease,pathy,caseId} = useParams()
-  const navigate=useNavigate()
+  const { disease, pathy, caseId } = useParams();
+  const navigate = useNavigate();
   const [data, setData] = useState();
   useEffect(() => {
     getData();
   }, []);
   const getData = async () => {
     try {
-      // const res=await axios.get(`http://backend.healthumbrella.org:8000/disease/${disease}/${pathy}/directCase/${caseId}`);
       const res = await axios.get(
-        `http://backend.healthumbrella.org:8000/disease/migraine/acupressure/directCase/001`
+        `http://backend.healthumbrella.org:8000/disease/${disease}/${pathy}/directCase/${caseId}`
       );
+      // const res = await axios.get(
+      // `http://backend.healthumbrella.org:8000/disease/migraine/acupressure/directCase/001`
+      // );
 
       // console.log(res);
       setData(res.data);
@@ -23,12 +27,24 @@ function DirectCases() {
       console.log(e);
     }
   };
+
+  const capitalizedDisease = `${disease.charAt(0).toUpperCase()}${disease.slice(
+    1
+  )}`;
+  const capitalizedPathy = `${pathy.charAt(0).toUpperCase()}${pathy.slice(1)}`;
+  const concatenatedString = `${capitalizedDisease}/${capitalizedPathy}`;
+
+  const handleNavigateBack = () => {
+    navigate(-1);
+  };
+
   console.log(data);
   return (
     <div className="D_outer">
       <div className="D_outer_top">
-        <h2>&#60; Migraine/Accupressure</h2>
-        {/* <h2 onClick={()=>{navigate(-1)}}>&#60; `${disease}${pathy}`</h2> */}
+        <h2 onClick={handleNavigateBack} style={{ cursor: "pointer" }}>
+          &lt; {concatenatedString}
+        </h2>
       </div>
       <div className="D_outer_bottom">
         <div className="D_main_heading">
@@ -47,43 +63,43 @@ function DirectCases() {
               <div className="D_detail_table">
                 <table className="D_detail_inner_table">
                   <tr className="TR">
-                    <td className="TD1">Name - </td>
+                    <td className="TD1">Name  </td>
                     <td className="TD2">
                       {data?.personalDetails?.name ?? "NA"}
                     </td>
                   </tr>
                   <tr className="TR">
-                    <td className="TD1">Age - </td>
+                    <td className="TD1">Age  </td>
                     <td className="TD2">
                       {data?.personalDetails?.age ?? "NA"}
                     </td>
                   </tr>
                   <tr className="TR">
-                    <td className="TD1">Sex - </td>
+                    <td className="TD1">Sex  </td>
                     <td className="TD2">
                       {data?.personalDetails?.sex ?? "NA"}
                     </td>
                   </tr>
                   <tr className="TR">
-                    <td className="TD1">Occupation - </td>
+                    <td className="TD1">Occupation  </td>
                     <td className="TD2">
                       {data?.personalDetails?.occupation ?? "NA"}
                     </td>
                   </tr>
                   <tr className="TR">
-                    <td className="TD1">Email ID - </td>
+                    <td className="TD1">Email ID  </td>
                     <td className="TD2">
                       {data?.personalDetails?.emailAddress ?? "NA"}
                     </td>
                   </tr>
                   <tr className="TR">
-                    <td className="TD1">Phone No - </td>
+                    <td className="TD1">Phone No  </td>
                     <td className="TD2">
                       {data?.personalDetails?.phoneNumber ?? "NA"}
                     </td>
                   </tr>
                   <tr className="TR">
-                    <td className="TD1">Region - </td>
+                    <td className="TD1">Region  </td>
                     <td className="TD2">
                       {data?.personalDetails?.region ?? "NA"}
                     </td>
@@ -103,32 +119,37 @@ function DirectCases() {
         </div>
         <div className="D_outer_cards">
           <div className="D_cards">
-            <div className="D_casehistory">
-              {" "}
+            
+          {data?.caseHistory&& <div className="D_medicalreport">
               <a
                 href={data?.caseHistory}
                 target="__blank"
                 className="D_linkStyle"
               >
-                Case History
+                {/* <FiDownload size={18} color="black" className="D_FiUpload" />{""} */}
+                <span style={{ marginLeft: '8px' }}>Case History</span>
               </a>
-            </div>
-            <div className="D_allergies">
-              {" "}
-              <a href={data?.allergies} target="__blank" className="D_linkStyle">
-                Allergies
+            </div>}
+            {data?.allergies&&<div className="D_medicalreport">
+              <a
+                href={data?.allergies}
+                target="__blank"
+                className="D_linkStyle"
+              >
+                {/* <FiDownload size={18} color="black" className="D_FiUpload" />{""} */}
+                <span style={{ marginLeft: '8px' }}>Allergies</span>
               </a>
-            </div>
-            <div className="D_medicalreport">
+            </div>}
+            {data?.medicalReport&&<div className="D_medicalreport">
               <a
                 href={data?.medicalReport}
                 target="__blank"
                 className="D_linkStyle"
               >
-                <FiDownload size={18} color="#000000" className="D_FiUpload" />{" "}
-                Medical Report
+                {/* <FiDownload size={18} color="black" className="D_FiUpload" />{""} */}
+                <span style={{ marginLeft: '8px' }}>Medical Report</span>
               </a>
-            </div>
+            </div>}
           </div>
         </div>
       </div>
