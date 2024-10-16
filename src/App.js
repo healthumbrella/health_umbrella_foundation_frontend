c26a701c3eccbfd7d3dc1d22a5fdb08621fcb1b1
Arun <arunofficial400@gmail.com>
2024-10-16 16:21:00 +0530

    Home_first_page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/home";
+import Home2 from "./Components/Version_2/Home_v2/Home";
 import Header from "./Components/Header/header";
+import Header2 from "./Components/Version_2/Header_v2/Header_v2"
 import Footer from "./Components/Footer/footer";
 import Ejournal from "./Components/ejournal/ejournal";
 import Books from "./Components/BooksPage/books";
@@ -26,13 +28,15 @@ function App() {
   
   return (
     <div className="App">  
-      <Header />
+      {/* <Header /> */}
+      <Header2 />
       <Routes>
-        <Route path="/" exact element={<Home />}></Route> 
+        {/* //starting from here version 1 to is being commented
+         <Route path="/" exact element={<Home />}></Route> 
         <Route path="/about-us" exact element={<Home />}></Route>
-        {/* <Route path="/diseases" exact element={<Diseases/>}></Route> */}
+        //this was commented <Route path="/diseases" exact element={<Diseases/>}></Route> 
         <Route path="/disease/:disease" exact element={<Diseases/>}></Route>
-        {/* <Route path="/disease/:disease" exact element={<Disease/>}></Route> */}
+        //this was commented <Route path="/disease/:disease" exact element={<Disease/>}></Route> 
         <Route path="/disease/:disease/:titles" exact element={<PathyPage/>}></Route>  
         <Route path="/disease/:disease/:pathy/directCase/:caseId" exact element={<DirectCases />}></Route> 
         <Route path="/disease/:disease/:title1/books" exact element={<Books />}></Route> 
@@ -49,13 +53,21 @@ function App() {
         <Route path="/clinics/" exact element={<Clinics />}></Route> 
         <Route path="/feedback" exact element = {<Feedback />}></Route>
 
-        {/* new disease section */}
+        //this was commented new disease section 
         <Route path="/diseases/:disease" element={<Disease/>}>
           <Route index element={<DiseaseInfo/>} />
           <Route path=":therapy" element={<PathyInfo/>} />
-        </Route>
+        </Route> */}
+
         
-      </Routes>     
+        
+
+      {/* version 2 routes */}
+      <Route path="/" exact element={<Home2/>}></Route>
+
+      
+      </Routes>  
+
       <Footer />
     </div>
   );
---
+++ src/Components/Version_2/Home_v2/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import First from './home_v2_first_page/Home_v2_first.jsx';
+
+
+const Home_v2 = () => {
+  return (
+    <div>
+      <First />
+    </div>
+  );
+};
+
+export default Home_v2;
