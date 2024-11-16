7a6a1475904682d14ed94dbac73700d9526061ae
Divyanshi2206 <agarwald264@gmail.com>
2024-11-17 01:44:24 +0530

    suggestions coming under the search bar of header removed

--- src/Components/Version_2/Header_v2/search_header_v2.jsx
+++ src/Components/Version_2/Header_v2/search_header_v2.jsx
@@ -1,32 +1,23 @@
 import React, { useState } from "react";
 import "./search_header_v2.css";
-import { useNavigate, NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function SearchBar(props) {
   const searchData = props.diseaselist;
-  const [resultData, setResultData] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const navigate = useNavigate();
 
-  // Handle input change and filter search results
+  // Handle input change
   const handleChange = (event) => {
     const searchInputData = event.target.value.toLowerCase();
     setSearchInput(searchInputData);
-
-    if (searchInputData === "") {
-      setResultData([]);
-    } else {
-      const newResultData = searchData.filter((value) =>
-        value.toLowerCase().includes(searchInputData)
-      );
-      setResultData(newResultData);
-    }
   };
 
   // Handle click and Enter key navigation
   const handleNavigation = () => {
     if (searchInput.trim() !== "") {
       navigate(`/disease/${searchInput}`);
+      setSearchInput(""); // Clear the search input
     }
   };
 
@@ -37,10 +28,6 @@ function SearchBar(props) {
     }
   };
 
-  const scrollToTopOnClick = () => {
-    window.scrollTo(0, 0);
-  };
-
   return (
     <div className="Search_cont">
       <div className="Search_out">
@@ -48,8 +35,9 @@ function SearchBar(props) {
           placeholder="Search"
           type="text"
           className="Search_out_input"
+          value={searchInput} // Bind the input value to the state
           onChange={handleChange}
-          onKeyDown={handleKeyDown} // Added onKeyDown event
+          onKeyDown={handleKeyDown}
         />
         <button className="Search_out_button1" onClick={handleNavigation}>
           Search
@@ -58,21 +46,6 @@ function SearchBar(props) {
           <i className="fa-solid fa-magnifying-glass"></i>
         </button>
       </div>
-
-      {resultData.length !== 0 && (
-        <div className="dataResult">
-          {resultData.slice(0, 5).map((value) => (
-            <NavLink
-              className="dataItem"
-              to={`/disease/${value}`}
-              key={value}
-              onClick={scrollToTopOnClick}
-            >
-              {value}
-            </NavLink>
-          ))}
-        </div>
-      )}
     </div>
   );
 }
